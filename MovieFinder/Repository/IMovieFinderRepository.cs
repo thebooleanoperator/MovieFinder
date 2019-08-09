af71a97466b77a94a51eb653387d9a23da1403ae
griff1000 <grifgering@gmail.com>
2019-08-08 21:19:44 -0700

    changed type to iqueryable

--- MovieFinder/Repository/IMovieFinderRepository.cs
+++ MovieFinder/Repository/IMovieFinderRepository.cs
@@ -10,7 +10,7 @@ namespace MovieFinder.Repository
         where TEntity : class 
     {
         TEntity Get(int id);
-        IEnumerable<TEntity> GetAll(); 
+        IQueryable<TEntity> GetAll(); 
         void Add(TEntity entity); 
         void Remove(TEntity entity); 
     }
