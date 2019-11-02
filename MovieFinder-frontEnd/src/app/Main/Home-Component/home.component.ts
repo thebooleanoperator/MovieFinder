23534fcb8a4a2392496447b062d87e043a48fd54
griff1000 <grifgering@gmail.com>
2019-11-02 16:49:58 -0700

    added media query

--- MovieFinder-frontEnd/src/app/Main/Home-Component/home.component.ts
+++ MovieFinder-frontEnd/src/app/Main/Home-Component/home.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { LikedMoviesService } from 'src/app/Services/liked-movies.service'
 import { UserService } from 'src/app/Services/user.service';
 import { MovieDto } from '../../DTO/movie.dto'
+import { UserDto } from 'src/app/DTO/user.dto';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    user: UserDto = JSON.parse(localStorage.getItem("userInfo"))
     public likedMovies: MovieDto[]; 
     public displayedColumns : string[] = ['Title', 'Genre', 'Director', 'Year', 'RunTime'];
     constructor(private likedMoviesService: LikedMoviesService, private userService: UserService){}
@@ -16,6 +18,6 @@ export class HomeComponent implements OnInit {
    ngOnInit() {
        this.likedMoviesService.getLikedMovies(this.userService.userInfo.userId).subscribe((response) => {
            this.likedMovies = response; 
-       })
+       });
    }
 }
