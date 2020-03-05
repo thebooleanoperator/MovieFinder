c008905f8f52d9316e78322cfcf37dfec9dacd33
griff1000 <grifgering@gmail.com>
2020-03-04 18:23:21 -0700

    added route guards to dashboard and movies

---
+++ MovieFinder-frontEnd/src/app/Services/auth-guard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { AuthService } from './auth-service';
+
+@Injectable({providedIn: "root"})
+export class AuthGuardService implements CanActivate {
+    constructor(private router: Router, private authService: AuthService) 
+    {    
+    }
+
+    canActivate(): boolean {
+        if (!this.authService.token) {
+            this.router.navigate(['/welcome']);
+            alert("Redirecting to Welcome Page. You must log on to view Movie Finder")
+            return false;
+        }
+        return true;
+    }
+}
--- MovieFinder-frontEnd/src/app/app-routing.module.ts
+++ MovieFinder-frontEnd/src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { LoginComponent } from './Outside/Welcome/Login/login.component';
 import { DashboardComponent } from './Main/Home/Dashboard-Component/dashboard.component';
 import { RegisterComponent } from './Outside/Welcome/Register/register.component';
 import { MoviesComponent } from './Main/Home/Movies-Component/movies.component';
+import { AuthGuardService } from './Services/auth-guard.service';
 
 const routes: Routes = [
     {path: 'welcome', component: WelcomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
-    {path: 'dashboard', component: DashboardComponent},
-    {path: 'movies', component: MoviesComponent},
+    {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuardService]},
+    {path: 'movies', component: MoviesComponent, canActivate: [AuthGuardService]},
 
     {path: '**', redirectTo:'welcome'}
 ];
