6dadb5033bdb3f5913f055dfea686bc597fe4522
griff1000 <grifgering@gmail.com>
2020-06-28 21:30:41 -0700

    working on deploying to lambda

---
+++ MovieFinderAPI/LambdaEntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace MovieFinder.Serverless
+{
+    public class LambdaFunction : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseApiGateway();
+        }
+    }
+}
