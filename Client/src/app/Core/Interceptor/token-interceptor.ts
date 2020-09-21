c9b475b86d9441f04f330f1bdba94922d4093ef0
griff1000 <grifgering@gmail.com>
2020-09-20 23:15:07 -0700

    fixed refresh resend

--- Client/src/app/Core/Interceptor/token-interceptor.ts
+++ Client/src/app/Core/Interceptor/token-interceptor.ts
@@ -9,9 +9,37 @@ import { AppUtilities } from '../Utilities/app-utilities';
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
     constructor(public authService: AuthService, private _appUtils: AppUtilities){}
-    refreshingToken: boolean;
-
+    // Data
     private _refreshSubject: Subject<any> = new Subject<any>();
+
+    // Methods
+    public intercept(request: HttpRequest<any>,  next: HttpHandler): Observable<HttpEvent<any>> {
+        request = this.updateHeader(request);
+        // We do not want to catch error and resend for refresh token, login, or register.
+        // Refresh token is not being sent for these reqeusts, so this will prevent infinte loop.
+        if (request.url.endsWith("/RefreshToken") || request.url.endsWith("/Login") || request.url.endsWith("/Register")) {
+            return next.handle(request);
+        }
+        else {
+            return next.handle(request).pipe(
+                catchError((error, caught) => {
+                    if (error instanceof HttpErrorResponse) {
+                        if (error.status == 401) {
+                            return this.tokenExpired().pipe(
+                                switchMap(() => {
+                                    return next.handle(this.updateHeader(request, false));
+                                })
+                              );
+                        }
+                    }
+                    else {
+                        return throwError(error);
+                    }
+                    return caught; 
+                })
+            )
+        }
+    }
 
     private tokenExpired(): Subject<any> {
         this._refreshSubject.subscribe({
@@ -25,40 +53,23 @@ export class TokenInterceptor implements HttpInterceptor {
         return this._refreshSubject;
       }
 
-    public intercept(request: HttpRequest<any>,  next: HttpHandler): Observable<HttpEvent<any>> {
-        request = this.updateHeader(request);
-        // We do not want to catch error and resend for refresh token, login, or register.
-        // Refresh token is not being sent for these reqeusts, so this will prevent infinte loop.
-        if (request.url.endsWith("/RefreshToken") || request.url.endsWith("/Login") || request.url.endsWith("/Register")) {
-            return next.handle(request);
+    private updateHeader(request, withUrl=true) {
+        var url = this._appUtils.BaseUrl == "http://localhost:4200" ? "http://localhost:5001" : "https://pk57k8k0k3.execute-api.us-east-2.amazonaws.com/Prod";
+        if (withUrl) {
+            request = request.clone({
+                url: url + request.url,
+                setHeaders: {
+                    Authorization: `Bearer ${this.authService.token}`
+                }
+            });
         }
         else {
-            return next.handle(request).pipe(
-                catchError((error) => {
-                    if (error instanceof HttpErrorResponse) {
-                        if (error.status == 401) {
-                            return this.tokenExpired().pipe(
-                                switchMap(() => {
-                                  return next.handle(this.updateHeader(request));
-                                })
-                              );
-                        }
-                    }
-                    return throwError(error);
-                })
-            )
+            request = request.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${this.authService.token}`
+                }
+            });
         }
-    }
-
-    updateHeader(request) {
-        var url = this._appUtils.BaseUrl == "http://localhost:4200" ? "http://localhost:5001" : "https://pk57k8k0k3.execute-api.us-east-2.amazonaws.com/Prod";
-        request = request.clone({
-            url: url + request.url, 
-            withCredentials: true, // Needed to allow access to send cookies.
-            setHeaders: {
-                Authorization: `Bearer ${this.authService.token}`
-            }
-        });
         return request;
     }
 }
